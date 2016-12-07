367772956165b2579325371755af91acef937649
Md Nadim Ahmed <ahmed.nadim59@gmail.com>
2016-12-08 01:37:06 +1100

    030 Registration - Initial Steps

--- routes/index.js
+++ routes/index.js
@@ -17,4 +17,37 @@ router.get('/dashboard', function(req, res, next) {
     });
 });
 
+router.post('/register', function (req, res, next) {
+    // Getting the data from the form
+    var name = req.body.name;
+    var email = req.body.email;
+    var username = req.body.username;
+    var password = req.body.password;
+    var password2 = req.body.password2;
+
+    // validation checks
+    req.checkBody('name', 'Name field is required').notEmpty();
+    req.checkBody('email', 'Email field is required').notEmpty();
+    req.checkBody('username', 'Username field is required').notEmpty();
+    req.checkBody('password', 'Password field is required').notEmpty();
+    req.checkBody('email', 'Email must be a valid email address').isEmail();
+    req.checkBody('password2', 'Passwords do not match').equals(req.body.password);
+
+    var errors = req.validationErrors();
+
+    if (errors) {
+        res.render('register', {
+            errors: errors
+        });
+    }
+    else {
+        passport.authenticate('local-login', {
+            successRedirect: '/dashboard',
+            failureRedirect: '/',
+            failureFlash: true
+        })(req, res, next)
+    }
+
+});
+
 module.exports = router;
